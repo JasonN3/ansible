3da445d9a80fffb93d01ddc19c51ee889f5ad711
JasonN3 <33561705+JasonN3@users.noreply.github.com>
2023-01-16 14:48:52 -0600

    bug fixes

--- playbooks/satellite/run.sh
+++ playbooks/satellite/run.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+ansibleversion=$(ansible --version | grep 'python version' | cut -d= -f2 | awk '{print $1}' | cut -d. -f1,2)
+
+pip${ansibleversion} install jmespath
+
 mkdir -p ./collections/ansible_collections
 ansible-galaxy collection install -r requirements.yaml --collections-path ./collections/ansible_collections
 ansible-playbook main.yaml $@
