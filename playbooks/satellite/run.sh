02c298562b460a2f2c77074ad254aea9ab08d4a2
JasonN3 <33561705+JasonN3@users.noreply.github.com>
2023-01-26 13:20:56 -0600

    fixes

--- playbooks/satellite/run.sh
+++ playbooks/satellite/run.sh
@@ -4,8 +4,8 @@ set -ex
 
 ansibleversion=$(ansible --version | grep 'python version' | cut -d= -f2 | awk '{print $1}' | cut -d. -f1,2)
 
-pip${ansibleversion} install jmespath
+pip${ansibleversion} install jmespath dnspython
 
 mkdir -p ./collections/ansible_collections
 ansible-galaxy collection install -r requirements.yaml --collections-path ./collections/ansible_collections
-ansible-playbook main.yaml $@
+ansible-playbook -e CONFIG=configure.yaml main.yaml $@
